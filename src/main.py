e8e37604881ccf9ddb125d96e05563d2ed346875
gppc5096 <prsonamu@gmail.com>
2024-08-20 09:54:10 +0900

    Final Source

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from stock_currency import StockMonitorApp
-from PyQt5.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ex = StockMonitorApp()
-    ex.show()
-    sys.exit(app.exec_())
